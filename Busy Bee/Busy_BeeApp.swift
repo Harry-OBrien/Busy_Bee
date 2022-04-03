ff6d3addd36bfcd3e351b7683c1b031859742cea
Harry-OBrien <34553387+Harry-OBrien@users.noreply.github.com>
2022-04-03 18:31:45 +0100

    Initial Commit

---
+++ Busy Bee/Busy_BeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Busy_BeeApp.swift
+//  Busy Bee
+//
+//  Created by Harry O'Brien on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Busy_BeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
